a6175c04a8494df825f003c7f6ddbd52951ed5fc
Matheus G. Dias <matheusgiga123@gmail.com>
2025-07-28 13:39:52 -0300

    feat: create page navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './_components/navbar/navbar'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('generate-cert');
+  protected readonly showNavbar = false;
 }
