43ec71979af7327d533ccf337f07b4c3b4815746
Matheus G. Dias <matheusgiga123@gmail.com>
2025-07-28 17:01:17 -0300

    feat: create the secondary button

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,18 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './_components/navbar/navbar'
 import {PrimaryButton} from './_components/primary-button/primary-button'
+import {SecondaryButton} from './_components/secondary-button/secondary-button'
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    CommonModule,
+    PrimaryButton,
+    SecondaryButton
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
